e2d37f2cbd5933ecf390da700a78c26e590a7f08
Maximilian Krüger <kruemaxi@googlemail.com>
2016-02-26 20:51:46 +0100

    remove complexity of generics and make it compile

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,15 @@ fn main() {
 ```
 */
 
-use std::ops::{Add, Sub, Mul, Div, IndexMut};
+use std::ops::{Add, Sub, Mul, Div, IndexMut, Range};
+use std::cmp::{PartialOrd};
 
 #[macro_use]
 extern crate nalgebra;
 use nalgebra::ApproxEq;
 
 extern crate num;
-use num::{Float, Zero, NumCast, ToPrimitive};
+use num::{Float, Zero, NumCast, ToPrimitive, FromPrimitive};
 
 extern crate hertz;
 extern crate apodize;
@@ -35,66 +36,56 @@ extern crate apodize;
 extern crate itertools;
 use itertools::linspace;
 
-macro_rules! from {
-    ($expr:expr, $src:ty, $dst:ty) => {
-        <$dst as std::convert::From<$src>>::from($expr)
+macro_rules! f64_from_usize {
+    ($val:expr) => {
+        <f64>::from_usize($val)
+            .expect("type `f64` can't represent a specific value of type `usize` on this architecture.");
+    }
+}
+
+macro_rules! usize_from_f64 {
+    ($val:expr) => {
+        <usize>::from_f64($val)
+            .expect("type `usize` can't represent a specific value of type `f64` on this architecture.");
     }
 }
 
 #[inline]
-pub fn hertz_from_mel<T>(mel: T) -> T
-    where T: std::convert::From<f32> + Float + Div<T, Output=T> + Sub<T, Output=T>
-{
-    from!(700., f32, T) *
-        (from!(10., f32, T).powf(
-            mel / from!(2595., f32, T)) - from!(1., f32, T))
+pub fn hertz_from_mel(mel: f64) -> f64 {
+    700. * ((10.).powf(mel / 2595.) - 1.)
 }
 
 #[inline]
-pub fn mel_from_hertz<T>(hertz: T) -> T
-    where T: std::convert::From<f32> + Float + Mul<T, Output=T> + Add<T, Output=T>
-{
-    from!(2595., f32, T) *
-        (from!(1., f32, T) + hertz / from!(700., f32, T)).log10()
-}
-
-macro_rules! test_mel {
-    ($float:ty) => {{
-        assert_approx_eq_eps!(
-            549.64 as $float, mel_from_hertz(440 as $float), 0.01);
-        assert_approx_eq_eps!(
-            440 as $float, hertz_from_mel(549.64 as $float), 0.01);
-
-        let mel: $float = 0.;
-        assert_approx_eq!(mel, mel_from_hertz(hertz_from_mel(mel)));
-        let mel: $float = 100.;
-        assert_approx_eq_eps!(
-            mel, mel_from_hertz(hertz_from_mel(mel)), 0.0001);
-        let mel: $float = 3000.;
-        assert_approx_eq_eps!(
-            mel, mel_from_hertz(hertz_from_mel(mel)), 0.0001);
-
-        let hertz: $float = 0.;
-        assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
-        let hertz: $float = 1000.;
-        assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
-        let hertz: $float = 44100. / 2.;
-        assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
-    }}
+pub fn mel_from_hertz(hertz: f64) -> f64 {
+    2595. * (1. + hertz / 700.).log10()
 }
 
 #[test]
-fn test_mel_f32() {
-    test_mel!(f32);
-}
+fn test_mel() {
+    assert_approx_eq_eps!(
+        549.64, mel_from_hertz(440.), 0.01);
+    assert_approx_eq_eps!(
+        440., hertz_from_mel(549.64), 0.01);
 
-#[test]
-fn test_mel_f64() {
-    test_mel!(f64);
+    let mel = 0.;
+    assert_approx_eq!(mel, mel_from_hertz(hertz_from_mel(mel)));
+    let mel = 100.;
+    assert_approx_eq_eps!(
+        mel, mel_from_hertz(hertz_from_mel(mel)), 0.0001);
+    let mel = 3000.;
+    assert_approx_eq_eps!(
+        mel, mel_from_hertz(hertz_from_mel(mel)), 0.0001);
+
+    let hertz = 0.;
+    assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
+    let hertz = 1000.;
+    assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
+    let hertz = 44100. / 2.;
+    assert_approx_eq!(hertz, hertz_from_mel(mel_from_hertz(hertz)));
 }
 
 /// col index changes fastest
-struct MelScalingMatrixEnumerator<T, WindowIter> {
+pub struct MelScalingMatrixEnumerator<WindowIter> {
     // parameters
 
     /// equivalent to col count
@@ -102,8 +93,8 @@ struct MelScalingMatrixEnumerator<T, WindowIter> {
     /// equivalent to row count
     output_size: usize,
 
-    mel_from_hertz: fn(T) -> T,
-    hertz_from_mel: fn(T) -> T,
+    mel_from_hertz: fn(f64) -> f64,
+    hertz_from_mel: fn(f64) -> f64,
 
     window_function: fn(usize) -> WindowIter,
 
@@ -112,16 +103,17 @@ struct MelScalingMatrixEnumerator<T, WindowIter> {
     row_index: usize,
     col_index: usize,
 
-    start_mels_iter: itertools::Linspace<T>,
-    end_mels_iter: itertools::Linspace<T>,
+    start_mels_iter: itertools::Linspace<f64>,
+    end_mels_iter: itertools::Linspace<f64>,
 
     // this gets set anew for every row
+    window_start: usize,
     window_size: usize,
     window_iter: WindowIter
 }
 
-impl<T, WindowIter> MelScalingMatrixEnumerator<T, WindowIter>
-    where T: Zero
+impl<WindowIter> MelScalingMatrixEnumerator<WindowIter>
+    where WindowIter: Iterator<Item=f64>
 {
     #[inline]
     pub fn is_done(&self) -> bool {
@@ -150,27 +142,25 @@ impl<T, WindowIter> MelScalingMatrixEnumerator<T, WindowIter>
     }
 }
 
-impl<T, WindowIter> Iterator for MelScalingMatrixEnumerator<T, WindowIter>
-    where T: Zero
+impl<WindowIter> Iterator for MelScalingMatrixEnumerator<WindowIter>
+    where WindowIter: Iterator<Item=f64>
 {
-    type Item = T;
+    type Item = f64;
 
     #[inline]
-    fn next(&mut self) -> Option<T> {
+    fn next(&mut self) -> Option<f64> {
         if self.is_done() {
             return None;
         }
-        Some(T::zero())
-        //     Some(value) => {
-        //         if self.is_at_end_of_row() {
-        //             self.col_index = 0;
-        //             self.row_index += 1;
-        //         } else {
-        //             self.col_index += 1;
-        //         }
-        //         value
-        //     },
-        // }
+        if self.is_at_end_of_row() {
+            self.col_index = 0;
+            self.row_index += 1;
+            // TODO new window iterator...
+            return Some(0.);
+        }
+
+        self.col_index += 1;
+        Some(0.)
     }
 
         // // iterate row in outer loop (slow)
@@ -205,35 +195,13 @@ impl<T, WindowIter> Iterator for MelScalingMatrixEnumerator<T, WindowIter>
     }
 }
 
-impl<T, WindowIter> ExactSizeIterator for MelScalingMatrixEnumerator<T, WindowIter>
-    where T: Zero
+impl<WindowIter> ExactSizeIterator for MelScalingMatrixEnumerator<WindowIter>
+    where WindowIter: Iterator<Item=f64>
 {
     #[inline]
     fn len(&self) -> usize {
         self.input_size * self.output_size
     }
-}
-
-// TODO better name
-pub fn mel_scaling_matrix_indexes<T> (
-    mels: Range<T>,
-) -> Range<usize>
-{
-    let start_hertz = hertz_from_mel(mels.start);
-    let end_hertz = hertz_from_mel(mels.end);
-    assert!(start_hertz < end_hertz);
-
-    // divide by max_hertz
-    // multiply with output_size
-
-    let start_index = <usize as NumCast>::from()
-        .expect("type `usize` can't represent a certain value of type of `T`.
-                choose a `T` that can represent fewer numbers.");
-    let end_index = <usize as NumCast>::from()
-        .expect("type `usize` can't represent a certain value of type of `T`.
-                choose a `T` that can represent fewer numbers.");
-
-    start_index..end_index
 }
 
 /// (or many vectors at once in form of a matrix)
@@ -244,22 +212,20 @@ pub fn mel_scaling_matrix_indexes<T> (
 /// TODO get frequency index pair for nth mel
 /// TODO enumerate scaling vector
 /// TODO which dimension first
-pub fn enumerate_mel_scaling_matrix_base<T, WindowIter>(
+pub fn enumerate_mel_scaling_matrix_base<WindowIter>(
     // TODO more descriptive argument names
-    min_hertz: T,
-    max_hertz: T,
+    min_hertz: f64,
+    max_hertz: f64,
     // TODO shape
     input_size: usize,
     output_size: usize,
-    mel_from_hertz: fn(T) -> T,
-    hertz_from_mel: fn(T) -> T,
+    mel_from_hertz: fn(f64) -> f64,
+    hertz_from_mel: fn(f64) -> f64,
     window_function: fn(usize) -> WindowIter
-) -> MelScalingMatrixEnumerator<T, WindowIter>
-    where WindowIter: Iterator<Item=T>,
-          T: std::cmp::PartialOrd + Float + ToPrimitive + FromPrimitive
-          usize: itertools::misc::ToFloat<T>
+) -> MelScalingMatrixEnumerator<WindowIter>
+    where WindowIter: Iterator<Item=f64>
 {
-    assert!(min_freq < max_freq);
+    assert!(min_hertz < max_hertz);
     assert!(output_size < input_size);
     assert!(0 < output_size);
     assert!(0 < input_size);
@@ -271,55 +237,57 @@ pub fn enumerate_mel_scaling_matrix_base<T, WindowIter>(
     let mut end_mels_iter = linspace(max_mel, max_mel, output_size);
     end_mels_iter.next();
 
-    let first_window_start_mel = start_mels_iter.next().unwrap();
-    let first_window_end_mel= end_mels_iter.next().unwrap();
-    assert!(first_window_start_mel < first_window_end_mel);
-    let first_indexes = mel_scaling_matrix_indexes(
-        first_window_start_mel..first_window_end_mel);
+    let start_mel = start_mels_iter.next().unwrap();
+    let end_mel = end_mels_iter.next().unwrap();
 
+    let start_hertz = hertz_from_mel(start_mel);
+    let end_hertz = hertz_from_mel(end_mel);
 
-    let first_window_size = first_window_end_hertz - first_window_start_hertz;
+    // TODO maybe round or floor or ceil here
+    let start_index = usize_from_f64!(
+        start_hertz / max_hertz * f64_from_usize!(output_size));
 
-    let first_window_iter = window_function(first_window_size);
+    let end_index = usize_from_f64!(
+        end_hertz / max_hertz * f64_from_usize!(output_size));
 
-    MelScalingMatrixEnumerator::<T, WindowIter> {
+    let window_size = end_index - start_index;
+
+    let window_iter = window_function(window_size);
+
+    MelScalingMatrixEnumerator::<WindowIter> {
         // parameters
         input_size: input_size,
         output_size: output_size,
         window_function: window_function,
+        hertz_from_mel: hertz_from_mel,
+        mel_from_hertz: mel_from_hertz,
 
         // state
         row_index: 0,
         col_index: 0,
+
         start_mels_iter: start_mels_iter,
         end_mels_iter: end_mels_iter,
-        window_size: first_window_size,
-        window_iter: first_window_iter,
+
+        window_start: start_index,
+        window_size: window_size,
+        window_iter: window_iter,
     }
 }
 
 /// one filter per row
-pub fn enumerate_mel_scaling_matrix<T, T1, T2>(
-    sample_rate: T1,
-    window_size: T2,
+pub fn enumerate_mel_scaling_matrix(
+    sample_rate: usize,
+    window_size: usize,
     input_size: usize,
     output_size: usize,
-) -> MelScalingMatrixEnumerator<T, apodize::TriangularWindowIter>
-    where T: NumCast,
-          T1: ToPrimitive,
-          T2: ToPrimitive,
+) -> MelScalingMatrixEnumerator<apodize::TriangularWindowIter>
 {
-    let cast_sample_rate = <T as NumCast>::from(sample_rate)
-        .expect("type `T` can't represent a certain value of type of `T1`.
-                choose a `T1` that can represent fewer numbers
-                or `T` that can represent more numbers.");
-    let cast_window_size = <T as NumCast>::from(window_size)
-        .expect("type `T` can't represent a certain value of type of `T2`.
-                choose a `T2` that can represent fewer numbers
-                or `T` that can represent more numbers.");
     enumerate_mel_scaling_matrix_base(
-        hertz::rayleigh(cast_sample_rate, cast_window_size),
-        hertz::nyquist(cast_sample_rate),
+        hertz::rayleigh(
+            f64_from_usize!(sample_rate),
+            f64_from_usize!(window_size)),
+        hertz::nyquist(f64_from_usize!(sample_rate)),
         input_size,
         output_size,
         mel_from_hertz,
