e7af6c154d6d6154d36de66f9d0288d602b8aa95
Travis CI User <travis@example.org>
2017-10-01 11:52:02 +0000

    Update documentation

---
+++ implementors/core/iter/traits/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mel"] = ["impl&lt;WindowIter&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"mel/struct.MelScalingMatrixEnumerator.html\" title=\"struct mel::MelScalingMatrixEnumerator\">MelScalingMatrixEnumerator</a>&lt;WindowIter&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;WindowIter: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a>&lt;Item = <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.f64.html\">f64</a>&gt;,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
